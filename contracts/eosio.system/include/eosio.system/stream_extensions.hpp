8832d1078b76c52d52cd9d8e99f7c49e9e8be125
Todd Fleming <todd.fleming@block.one>
2020-07-23 09:41:18 -0400

    vote pool

--- contracts/eosio.system/include/eosio.system/stream_extensions.hpp
+++ contracts/eosio.system/include/eosio.system/stream_extensions.hpp
@@ -7,7 +7,7 @@ namespace eosiosystem {
    inline bool something_has_value() { return false; }
 
    template <typename T, typename... Ts>
-   bool something_has_value(const T& obj, const Ts&... objs) {
+   bool something_has_value(const eosio::binary_extension<T>& obj, const Ts&... objs) {
       if (obj.has_value())
          return true;
       return something_has_value(objs...);
@@ -18,10 +18,15 @@ namespace eosiosystem {
       return ds;
    }
 
+   // Similar to `ds << ...`, but only supports binary_extensions and leaves off unfilled ones at the end.
+   // Also avoids a CDT 1.7 issue that copies inner vectors before serializing them.
    template <typename DataStream, typename T, typename... Ts>
-   DataStream& write_extensions(DataStream& ds, const T& obj, const Ts&... objs) {
+   DataStream& write_extensions(DataStream& ds, const eosio::binary_extension<T>& obj, const Ts&... objs) {
       if (something_has_value(obj, objs...)) {
-         ds << obj;
+         if (obj.has_value())
+            ds << obj.value();
+         else
+            ds << T{};
          return write_extensions(ds, objs...);
       }
       return ds;
