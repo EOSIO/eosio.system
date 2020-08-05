be871a2ad66b52cacda3cccf942d2d7909bd6053
Todd Fleming <todd.fleming@block.one>
2020-08-05 18:49:48 -0400

    testing

--- tests/eosio.votepool_tests.cpp
+++ tests/eosio.votepool_tests.cpp
@@ -35,6 +35,41 @@ struct votepool_tester : eosio_system_tester {
       BOOST_REQUIRE_EQUAL(success(), vote(N(bp11activate), { N(bp11activate) }));
       BOOST_REQUIRE_EQUAL(success(), vote(N(bp11activate), {}));
    }
+
+   action_result initvpool(name authorizer, const std::vector<uint32_t>& durations) {
+      return push_action(authorizer, N(initvpool), mvo()("durations", durations));
+   }
+
+   action_result cfgvpool(name authorizer, double prod_rate, double voter_rate) {
+      return push_action(authorizer, N(cfgvpool), mvo()("prod_rate", prod_rate)("voter_rate", voter_rate));
+   }
+
+   action_result stake2pool(name authorizer, name owner, uint32_t pool_index, asset amount) {
+      return push_action(authorizer, N(stake2pool), mvo()("owner", owner)("pool_index", pool_index)("amount", amount));
+   }
+
+   action_result claimstake(name authorizer, name owner, uint32_t pool_index, asset requested) {
+      return push_action(authorizer, N(claimstake),
+                         mvo()("owner", owner)("pool_index", pool_index)("requested", requested));
+   }
+
+   action_result transferstake(name authorizer, name from, name to, uint32_t pool_index, asset requested,
+                               const std::string& memo) {
+      return push_action(authorizer, N(transferstake),
+                         mvo()("from", from)("to", to)("pool_index", pool_index)("requested", requested)("memo", memo));
+   }
+
+   action_result updatevotes(name authorizer, name user, name producer) {
+      return push_action(authorizer, N(updatevotes), mvo()("user", user)("producer", producer));
+   }
+
+   action_result updatepay(name authorizer, name user) {
+      return push_action(authorizer, N(updatepay), mvo()("user", user));
+   }
+
+   action_result claimvotepay(name authorizer, name producer) {
+      return push_action(authorizer, N(claimvotepay), mvo()("producer", producer));
+   }
 };
 
 BOOST_AUTO_TEST_SUITE(eosio_system_votepool_tests)
@@ -42,6 +77,7 @@ BOOST_AUTO_TEST_SUITE(eosio_system_votepool_tests)
 BOOST_AUTO_TEST_CASE(votepool_tests) try {
    {
       votepool_tester t;
+      BOOST_REQUIRE_EQUAL(t.success(), t.initvpool(N(eosio), { 6 * 4, 12 * 4, 24 * 4 }));
    }
 } // votepool_tests
 FC_LOG_AND_RETHROW()
